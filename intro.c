6334ebeecb37093e604c82bd75c76bb02e5916ad
adheesha_ravindu <162187280+ZacScorpiOO@users.noreply.github.com>
2024-03-28 10:48:27 +0000

    Update intro.c

--- intro.c
+++ intro.c
@@ -1,18 +1,22 @@
-
-
-/*C is like the building blocks of Legos for the computer world! Created in the 1970s by Dennis Ritchie, it's a fundamental programming language that's still widely used today.
+/*C is like the building blocks of Legos for the computer world! Created in the 1970s by Dennis Ritchie, it's a fundamental programming
+language that's still widely used today.
 
 Here's what makes C special:
 
-    *Powerful and Precise: C gives programmers a lot of control over how the computer works, making it great for building high-performance applications and system software like operating systems.
+    *Powerful and Precise: C gives programmers a lot of control over how the computer works, making it great for building high-performance 
+	 applications and system software like operating systems.
 
-    *The Foundation for Many Languages: If you learn C, understanding other popular languages like Java, Python, or C++ becomes much easier because they share similar concepts.
+    *The Foundation for Many Languages: If you learn C, understanding other popular languages like Java, Python, or C++ becomes much easier
+	 because they share similar concepts.
 
-    *Speed Demon: Compared to some other languages, C can be incredibly fast. This makes it ideal for situations where speed is critical, like in game development or embedded systems.
+    *Speed Demon: Compared to some other languages, C can be incredibly fast. This makes it ideal for situations where speed is critical, 
+	 like in game development or embedded systems.
 
-    *Versatile and Adaptable: C can be used to create a wide variety of software, from tiny programs to massive operating systems. It's a true all-rounder in the programming world.
+    *Versatile and Adaptable: C can be used to create a wide variety of software, from tiny programs to massive operating systems. It's 
+	 a true all-rounder in the programming world.
 
-Learning C might seem old-school, but it's like mastering the alphabet before writing a novel. It gives you a strong foundation for understanding how computers work and lays the groundwork for exploring other programming languages.
+Learning C might seem old-school, but it's like mastering the alphabet before writing a novel. It gives you a strong foundation for 
+understanding how computers work and lays the groundwork for exploring other programming languages.
 
 To start coding C you need a text editor and a compiler.
 This tutorial is doing using a IDE(Code::Blocks),
