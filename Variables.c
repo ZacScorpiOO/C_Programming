9705ed2ad04b9809d032b52fafd151ee0cb817b9
ZacScorpiOO <adheeshazxy@gmail.com>
2024-03-28 16:45:51 +0530

    Create Variables.c

---
+++ Variables.c
@@ -0,0 +1,33 @@
+
+
+/*C is like the building blocks of Legos for the computer world! Created in the 1970s by Dennis Ritchie, it's a fundamental programming language that's still widely used today.
+
+Here's what makes C special:
+
+    *Powerful and Precise: C gives programmers a lot of control over how the computer works, making it great for building high-performance applications and system software like operating systems.
+
+    *The Foundation for Many Languages: If you learn C, understanding other popular languages like Java, Python, or C++ becomes much easier because they share similar concepts.
+
+    *Speed Demon: Compared to some other languages, C can be incredibly fast. This makes it ideal for situations where speed is critical, like in game development or embedded systems.
+
+    *Versatile and Adaptable: C can be used to create a wide variety of software, from tiny programs to massive operating systems. It's a true all-rounder in the programming world.
+
+Learning C might seem old-school, but it's like mastering the alphabet before writing a novel. It gives you a strong foundation for understanding how computers work and lays the groundwork for exploring other programming languages.
+
+To start coding C you need a text editor and a compiler.
+This tutorial is doing using a IDE(Code::Blocks),
+so windows - Dev-C++
+Linux - Code::Blocks
+Mac - Dev-C++    etc....
+
+Lets Print Hello World*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    //output
+    printf("Hello world!\n");
+    return 0;
+}
