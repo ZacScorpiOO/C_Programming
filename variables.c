9ff76ba3846ef9e607261614dc27457476c174e5
Ravindu Adheesha <162187280+adheesharavindu@users.noreply.github.com>
2024-03-28 12:51:02 +0000

    Update variables.c

--- variables.c
+++ variables.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-int main()
-{
+int main(){
 int x = 5; // Integer (whole number)
 float y = 5.12; // decimals (around 6-7 decimal digits before losing precision)
 double yz = 5.12433423243535454 ;// decimal places (around 15-16 decimal digits without losing precision)
@@ -10,5 +9,4 @@ printf("%d\n",x);
 printf("%.2f\n",y);
 printf("%.10lf", yz);
 printf("\n%c",'z');
-return 0;
-}
+return 0;}
